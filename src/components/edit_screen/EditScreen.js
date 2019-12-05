ee82b06070e059e20fa2a6baf0d9432abc2e23ce
fllp <fllp@foxmail.com>
2019-12-05 12:47:43 -0500

    Add Wireframe Div
    -with some additional css styling

--- src/components/edit_screen/EditScreen.js
+++ src/components/edit_screen/EditScreen.js
@@ -46,48 +46,51 @@ class EditScreen extends Component {
             }
         }
         return (
-            <div className="container white">
-                <h5 className="grey-text text-darken-3">
-                    <i class="small zoom material-icons">zoom_in</i>
-                    <i class="small zoom material-icons">zoom_out</i>
-                    &nbsp;&nbsp;
+            <div className="container">
+                <div className="toolbar">
+                    <h5 className="grey-text text-darken-3">
+                        <i className="small zoom material-icons">zoom_in</i>
+                        <i className="small zoom material-icons">zoom_out</i>
+                        &nbsp;&nbsp;
                     <Button className="save-btn waves-effect">Save</Button>
-                    &nbsp;
+                        &nbsp;
                     <Link to="/"><Button className="close-btn waves-effect">Close</Button></Link>
-                    &nbsp;&nbsp;
+                        &nbsp;&nbsp;
                 <Modal
-                        header="Confirm Deleting Wireframe"
-                        trigger={<p className="btn trashcan">🗑</p>}
-                        actions={
-                            <div>
-                                <Button modal="close" className="red darken-2" onClick={this.handleWireframeDelete}>Confirm</Button>
-                                &nbsp;&nbsp;&nbsp;
+                            header="Confirm Deleting Wireframe"
+                            trigger={<p className="btn trashcan">🗑</p>}
+                            actions={
+                                <div>
+                                    <Button modal="close" className="red darken-2" onClick={this.handleWireframeDelete}>Confirm</Button>
+                                    &nbsp;&nbsp;&nbsp;
                                 <Button modal="close">dismiss</Button>
-                            </div>
-                        }
-                    >
-                        <h5>Are you sure you want to delete this Wireframe?</h5>
-                    </Modal>
-                </h5>
-                <div className="input-field">
-                    <label htmlFor="email" className={wireframe.name === "" ? "" : "active"}>Name</label>
-                    <input className="active" type="text" name="name" id="name" onChange={this.handleChange} value={wireframe.name} />
-                </div>
-                <div class="control-collection">
-                    <div className="rectangle control-demo"></div>
-                    <label>Container</label>
-                    <br/><br/>
-                    <div className="control-demo">Prompt for Input:</div>
-                    <label>Label</label>
-                    <br/><br/>
-                    <button className="button-demo control-demo" disabled>Submit</button><br/>
-                    <label>Button</label>
-                    <br/><br/>
-                    <input className="input-demo control-demo" placeholder='Input' disabled></input>
-                    <br/>
-                    <label>Textfield</label>
-                </div>
-            </div >
+                                </div>
+                            }
+                        >
+                            <h5>Are you sure you want to delete this Wireframe?</h5>
+                        </Modal>
+                    </h5>
+                    <div className="input-field">
+                        <label htmlFor="email" className={wireframe.name === "" ? "" : "active"}>Name</label>
+                        <input className="active" type="text" name="name" id="name" onChange={this.handleChange} value={wireframe.name} />
+                    </div>
+                    <div className="control-collection">
+                        <div className="rectangle control-demo"></div>
+                        <label>Container</label>
+                        <br /><br />
+                        <div className="control-demo">Prompt for Input:</div>
+                        <label>Label</label>
+                        <br /><br />
+                        <button className="button-demo control-demo" disabled>Submit</button><br />
+                        <label>Button</label>
+                        <br /><br />
+                        <input className="input-demo control-demo" placeholder='Input' disabled></input>
+                        <br />
+                        <label>Textfield</label>
+                    </div>
+                </div><br/>
+                <div className="wireframe"></div>
+            </div>
         );
     }
 }
